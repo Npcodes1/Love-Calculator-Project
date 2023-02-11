c774ec8531f94f5442ed7731aa8da1c95533b665
npcodes1 <nicole1rock@gmail.com>
2023-02-11 12:36:26 -0600

    Adding Love Message to Website

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ function calculate() {
 
     loveScore = Math.floor(loveScore) + 1;
 
-    if (loveScore < 100 && loveScore >= 75) {
-    alert ("Your Love Score is " + loveScore + "%" + ". You two are made for each other!");
+    if (loveScore <= 100 && loveScore >= 80) {
+    document.getElementById('message').innerHTML = "Your Love Score is " + loveScore + "%" + ". You two are made for each other!";
 } 
 
-    if (loveScore < 75 && loveScore >= 50) {
-    alert ("Your Love Score is " + loveScore + "%" + ". Your love can go either way. The choice is yours.");
+    if (loveScore <= 79 && loveScore >= 59) {
+    document.getElementById('message').innerHTML = "Your Love Score is " + loveScore + "%" + ". Your love can go either way. The choice is yours.";
 }
 
-    if (loveScore < 50 && loveScore >=1) {
-    alert ("Your Love Score is " + loveScore + "%" + ". You might want to rethink this love. Run away now!");
+    if (loveScore <= 58 && loveScore >= 38) {
+    document.getElementById('message').innerHTML = "Your Love Score is " + loveScore + "%" + ". Your love's not looking too hot.";
+}
+
+    if (loveScore <=37 && loveScore >=1) {
+    document.getElementById('message').innerHTML = "Your Love Score is " + loveScore + "%" + ". You might want to rethink this love. Run away now!";
 }
 
 }
